98c5edf17afc937b7c4f8e1fd7ba08ba246a58e1
Antoine Lair <62932641+LAIR-Antoine@users.noreply.github.com>
2024-02-19 10:00:14 +0100

    fix : style avancement sup 100%

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -873,7 +873,7 @@
                     <div class="w3-container">
 
                         <div class="w3-light-grey w3-round-xlarge">
-                            <div class="w3-container w3-blue w3-round-xlarge" style="width:{{ $goal->avancement }}%">
+                            <div class="w3-container w3-blue w3-round-xlarge" style="width:{{ $goal->avancement <= 100 ? $goal->avancement : 100 }}%">
                                 {{ $goal->avancement }}%</div>
                         </div>
                     </div>
